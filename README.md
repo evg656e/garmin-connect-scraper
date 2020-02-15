f25f8c07d99e2e0769eef7191e3f3bc04fe1c0ba
evg656e <evg656e@gmail.com>
2020-02-15 23:47:37 +0300

    Inital commit

--- README.md
+++ README.md
@@ -1,2 +1,98 @@
 # garmin-connect-scraper
-Incremental Puppeteer based Garmin Connect scraper
+
+Incremental [Puppeteer](https://github.com/puppeteer/puppeteer) based Garmin Connect™ scraper.
+
+## Usage
+
+Minimal usage:
+```
+garmin-connect-scraper --username=login@mail.com --password=secret
+```
+
+This will download all your activities as ```activities.json``` file into current folder.
+
+To change the default download path or to download additional data, use config files (see [configuration](#Configuration) section below):
+```
+garmin-connect-scraper --config=myconfig.json --username=login@mail.com --password=secret
+```
+
+## Configuration
+
+```jsonc
+{
+    // some general options
+    "general": {
+        // delay (in milliseconds, 5000 default) to download data 
+        // the smaller the number, the faster the data will be downloaded, but you will probably get a timeout penalty from garmin site (reduce on your own risk)
+        "requestDelay": 3000,
+        // default pick policy (possible values are "notNull" or "all")
+        // by default all properties are picked, changing to "notNull" will skip properties with null values 
+        "defaultPickPolicy": "notNull",
+        // specifies base directory that can be used in file paths lookups
+        // the lookup syntax is {varName}, some default lookup variables are available:
+        // cwd - current working directory (default)
+        // homeDir - system home directory
+        // currentDate - date part of current datetime in ISO format
+        // currentTime - time part current datetime in ISO format
+        "baseDir": "{homeDir}/sports"
+    },
+    // you can save your credentials here not to pass it through CLI every time (not a good idea though)
+    "credentials": {
+        "username": "login@mail.com",
+        "password": "secret"
+    },
+    // data retrieval section
+    "activities": {
+        "search": {
+            // search parameters as they used on https://connect.garmin.com/modern/activities page
+            // for example, to retrieve all swimming activities, specify:
+            "parameters": {
+                "activityType": "swimming",
+                "activitySubType": "lap_swimming"
+            },
+            // limits the number of activities retrieved (unlimited by default, so that all activities will be retrieved)
+            "limit": 100,
+            // path to file where to save activities
+            // this file will be used on the next program invocations to retrieve only new activities
+            // default lookup variables (сwd, homeDir or baseDir) are available
+            "path": "{baseDir}/lap_swimming/index.json",
+            // explicitly sets the set of properties to retrieve from activities
+            // it is possible to rename properties using syntax "name as newName"
+            // it is possible to use nested properties using syntax "parent.childName as newName" (when using nested properties renaming is mandatory)
+            // example (only 4 data fields will be picked for each activity):
+            "pick": [
+                "activityId",
+                "activityName",
+                "startTimeLocal as startTime",
+                "activityType.typeKey as activityTypeKey"
+            ]
+        },
+        // additional data retrievals
+        // you can specify additional data to be retrieved per activity
+        // for example, by this, splits and activity details will be downloaded for each retrieved activity from the "search" section
+        "fetch": [
+            {
+                // data url to be retrieved
+                // you can use activity's properties as lookups
+                "url": "https://connect.garmin.com/modern/proxy/activity-service/activity/{activityId}/splits",
+                // path to file where to save data
+                // you can use default variables (e.g. cwd, homeDir or baseDir) and current activity's properties (e.g. activityId) as lookups in this path
+                "path": "{baseDir}/lap_swimming/splits/{activityId}.json"
+                // "pick" property as mentioned above is available here
+            },
+            {
+                "url": "https://connect.garmin.com/modern/proxy/activity-service/activity/{activityId}",
+                "path": "{baseDir}/lap_swimming/activity/{activityId}.json"
+            }
+        ]
+    }
+}
+```
+
+The configuration file must be valid JSON file, so be sure to remove the comments, if you use this snippet.
+
+## Links
+
+Some other similar projects:
+  * https://github.com/fsrc/agcs/blob/master/lib/garmin-connect.ls
+  * https://github.com/pe-st/garmin-connect-export/blob/develop/gcexport.py
---
+++ bin/main.js
@@ -0,0 +1,365 @@
+#!/usr/bin/env node --experimental-specifier-resolution=node --harmony-optional-chaining --harmony-nullish
+import { homedir } from 'os';
+import puppeteer from 'puppeteer';
+import minimist from 'minimist';
+import {
+    log,
+    setSilent,
+    exitWithError,
+    showHelp,
+    showVersion,
+    programOptionsInfo,
+    programOptionNames
+} from '../lib/cli';
+import {
+    setDefault,
+    evaluateTemplate,
+    createPickPath,
+    pick,
+    compileTemplate
+} from '../lib/core';
+import {
+    readJSONFileDefault,
+    writeJSONPath
+} from '../lib/fs'
+import {
+    validateCredentials,
+    validateConfig,
+    validateActivities
+} from '../lib/schema';
+import { buildUrl } from '../lib/url';
+
+function logged(fn, begin, end, error) {
+    begin = begin !== undefined ? compileTemplate(begin) : begin;
+    end = end !== undefined ? compileTemplate(end) : end;
+    error = error !== undefined ? compileTemplate(error) : error;
+
+    return async function (...args) {
+        try {
+            if (begin !== undefined)
+                log(begin(args));
+            const ret = await fn(...args);
+            if (end !== undefined)
+                log(end([...args, ret]));
+            return ret;
+        }
+        catch (err) {
+            if (error !== undefined)
+                log(error([...args, err]));
+            throw err;
+        }
+    };
+}
+
+function createDelayedQueue(delay) {
+    const queue = [];
+    let timerId = null;
+
+    function consume() {
+        timerId = null;
+        queue.pop();
+        if (queue.length !== 0)
+            consumeNext(queue[queue.length - 1][0]);
+    }
+
+    function consumeNext(callbackfn) {
+        callbackfn();
+        timerId = setTimeout(consume, delay);
+    }
+
+    return [
+        function enqueue(callbackfn, clearfn) {
+            queue.unshift([callbackfn, clearfn]);
+            if (queue.length === 1)
+                consumeNext(callbackfn);
+        },
+        function clear() {
+            if (timerId !== null) {
+                clearTimeout(timerId);
+                timerId = null;
+            }
+            while (queue.length !== 0) {
+                const clearfn = queue.pop()[1];
+                if (clearfn !== undefined)
+                    clearfn();
+            }
+        }
+    ];
+}
+
+const launchBrowser = logged(async function launchBrowser(config) {
+    const options = { ...config.puppeteer?.launchOptions };
+
+    const requestDelay = config.general?.requestDelay ?? 5000;
+    const [enqueue, clear] = createDelayedQueue(requestDelay);
+
+    const browser = await puppeteer.launch(options);
+    const page = await browser.newPage();
+    await page.setRequestInterception(true);
+    page.on('request', request => {
+        const headers = request.headers();
+        const { queued, hint } = headers;
+        if (queued) {
+            enqueue(() => {
+                log(`Fetching ${request.url()}${hint ? ' ' + hint : ''}...`);
+                delete headers.hint;
+                delete headers.queued;
+                request.continue({ headers });
+            });
+            return;
+        }
+        request.continue();
+    });
+
+    return [page, clear];
+}, 'Launching browser...', 'Browser launched.');
+
+async function evaluateFetch(page, url, hint = '') {
+    const ret = await page.evaluate(async (url, hint) => {
+        const response = await fetch(url, {
+            headers: {
+                queued: true,
+                hint
+            }
+        });
+        const json = await response.json();
+        return json;
+    }, url, hint);
+    log(`${url} fetched.`);
+    return ret;
+}
+
+const signin = logged(async function signin(page, { username, password }, remember = false) {
+    await page.goto('https://connect.garmin.com/signin', { waitUntil: 'networkidle0' });
+
+    const elementHandle = await page.$('#gauth-widget-frame-gauth-widget');
+    const frame = await elementHandle.contentFrame();
+
+    await frame.type('#username', username);
+    await frame.type('#password', password);
+
+    if (remember)
+        await frame.click('#login-remember');
+
+    const [response] = await Promise.all([
+        frame.waitForNavigation({ waitUntil: 'networkidle0' }),
+        frame.click('#login-btn-signin'),
+    ]);
+
+    if (!response.ok())
+        throw new Error(`Sign in failed, response code: ${response.status()}`);
+
+    await page.waitForNavigation({ waitUntil: 'networkidle0' });
+}, 'Signing in as {[1].username}...', 'Signed in.');
+
+function pickAll(obj) {
+    return { ...obj };
+}
+
+function pickNotNull(obj) {
+    return Object.fromEntries(Object.entries(obj).filter(([_, value]) => value != null));
+}
+
+function normalizePathes(pathes, defaults = []) {
+    const ret = pathes.slice();
+    for (const path of defaults) {
+        const index = ret.indexOf(path);
+        if (index === -1)
+            ret.unshift(path);
+    }
+    return ret;
+}
+
+function createPick(defaultPickPolicy, pathes, defaultPathes) {
+    if (pathes !== undefined)
+        return createPickPath(normalizePathes(pathes, defaultPathes));
+    switch (defaultPickPolicy.toLowerCase()) {
+        case 'notnull':
+            return pickNotNull;
+        case 'all':
+        default:
+            return pickAll;
+    }
+}
+
+const activitiesSearchBaseUrl = 'https://connect.garmin.com/modern/proxy/activitylist-service/activities/search/activities';
+
+const updateActivities = logged(async function updateActivities(page, searchParameters = {}, oldActivities = [], pick = pickAll, finish) {
+    const latestActivityId = oldActivities[0]?.activityId ?? -1;
+    const newActivities = [];
+
+    searchParameters = { start: 0, limit: 20, ...searchParameters };
+
+    let { start, limit } = searchParameters;
+
+    for (; finish === undefined || start < finish; start += limit) {
+        searchParameters.start = start;
+        searchParameters.limit = limit;
+
+        const activitiesChunk = await evaluateFetch(page, buildUrl(activitiesSearchBaseUrl, searchParameters));
+
+        if (activitiesChunk.length === 0
+            || activitiesChunk.some((activity) => {
+                if (latestActivityId === activity.activityId)
+                    return true;
+                newActivities.push(pick(activity));
+            }))
+            break;
+    }
+
+    return newActivities;
+}, 'Updating activities...', 'Activities updated, got {[-1].length} new activities.');
+
+
+const writeDetails = logged(function writeDetails(path, details) {
+    return writeJSONPath(path, details);
+}, 'Writing {0}...', '{0} written.');
+
+function fetchActivityDetails(page, newActivities, { url, path, pick }, env, defaultPickPolicy) {
+    const resolveUrl = compileTemplate(url);
+    const resolvePath = compileTemplate(path);
+    const pickDetails = createPick(defaultPickPolicy, pick);
+    return Promise.all(newActivities.map(async (activity, index) => {
+        const json = await evaluateFetch(page, resolveUrl(activity), newActivities.length > 1 ? `(${index + 1} of ${newActivities.length})` : '');
+        return writeDetails(resolvePath({ ...env, ...activity }), pickDetails(json));
+    }));
+}
+
+const fetchActivityDetailsAll = logged(function fetchActivityDetailsAll(page, newActivities, fetches, env, defaultPickPolicy) {
+    return Promise.all(fetches.map(fetch => fetchActivityDetails(page, newActivities, fetch, env, defaultPickPolicy)));
+}, 'Fetching activity details...', 'Activity details fetched.');
+
+function fetchActivityDetailsAllHelper(page, newActivities, fetches, env, defaultPickPolicy) {
+    if (fetches.length === 0)
+        return Promise.resolve();
+    return fetchActivityDetailsAll(page, newActivities, fetches, env, defaultPickPolicy);
+}
+
+const readActivities = logged(async function readActivities(path) {
+    return validateActivities(await readJSONFileDefault(path, []));
+}, 'Reading {0}...', '{0} read.');
+
+const writeActivities = logged(function writeActivities(path, activities) {
+    return writeJSONPath(path, activities);
+}, 'Writing {0}...', '{0} written.');
+
+async function updateActivitiesAll(page, { activities }, env, defaultPickPolicy) {
+    const {
+        search,
+        fetch
+    } = {
+        search: {},
+        fetch: [],
+        ...activities
+    };
+
+    const {
+        parameters,
+        finish,
+        path,
+        pick
+    } = {
+        parameters: {},
+        path: 'activities.json',
+        ...search
+    };
+
+    const resolvedPath = evaluateTemplate(path, { ...env, ...parameters });
+
+    const oldActivities = await readActivities(resolvedPath);
+    const newActivities = await updateActivities(page, parameters, oldActivities, createPick(defaultPickPolicy, pick, ['activityId']), finish);
+
+    if (newActivities.length === 0)
+        return;
+
+    return Promise.all([
+        writeActivities(resolvedPath, newActivities.concat(oldActivities)),
+        fetchActivityDetailsAllHelper(page, newActivities, fetch, env, defaultPickPolicy)
+    ]);
+}
+
+function parseOptions(args, optionsInfo) {
+    const options = {};
+
+    for (const [name, alias, _, placeholder, _default] of optionsInfo) {
+        if (alias)
+            setDefault(options, 'alias', {})[alias] = name;
+        if (placeholder)
+            setDefault(options, 'string', []).push(name);
+        else
+            setDefault(options, 'boolean', []).push(name);
+        if (_default !== undefined)
+            setDefault(options, 'default', {})[name] = _default;
+    }
+
+    return minimist(args, options);
+}
+
+const readConfig = logged(async function readConfig(path) {
+    return validateConfig(await readJSONFileDefault(path, {}));
+}, 'Reading config {0}...', 'Config read.');
+
+const closeBrowser = logged(function closeBrowser(page) {
+    return page.browser().close();
+}, 'Closing browser...', 'Browser closed.');
+
+function toPosixSep(path) {
+    return path.replace(/\\/g, '/');
+}
+
+function createEnv({ general }) {
+    const now = new Date().toISOString();
+    const T = now.indexOf('T');
+    const Z = -5;
+    const cwd = toPosixSep(process.cwd());
+    const env = {
+        currentDate: now.slice(0, T),
+        currentTime: now.slice(T + 1, Z),
+        cwd,
+        homeDir: toPosixSep(homedir())
+    };
+    env.baseDir = evaluateTemplate(general?.baseDir ?? cwd, env);
+    return env;
+}
+
+(async () => {
+    try {
+        const {
+            config: configKey,
+            help: helpKey,
+            version: versionKey,
+            username: usernameKey,
+            password: passwordKey,
+            silent: silentKey
+        } = programOptionNames;
+
+        const options = parseOptions(process.argv.slice(2), programOptionsInfo);
+
+        setSilent(options[silentKey]);
+
+        if (options[helpKey]) {
+            showHelp(0);
+        }
+
+        if (options[versionKey]) {
+            showVersion(0);
+        }
+
+        const config = await readConfig(options[configKey]);
+        const credentials = validateCredentials({ ...config.credentials, ...pick(options, [usernameKey, passwordKey]) });
+
+        const env = createEnv(config);
+
+        const defaultPickPolicy = config.general?.defaultPickPolicy ?? 'all';
+
+        const [page, clearRequestQueue] = await launchBrowser(config);
+        await signin(page, credentials);
+        await updateActivitiesAll(page, config, env, defaultPickPolicy);
+
+        clearRequestQueue();
+        await closeBrowser(page);
+    }
+    catch (err) {
+        exitWithError(err.message);
+    }
+})();
